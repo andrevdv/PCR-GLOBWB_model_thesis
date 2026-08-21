6de7a591602921da394d5d87e86e2644089a8f15
andrevdv <220902110+andrevdv@users.noreply.github.com>
2026-08-21 17:29:58 +0200

    bmi updates

--- model/bmiPcrglobwb.py
+++ model/bmiPcrglobwb.py
@@ -8,12 +8,13 @@ import numpy as np
 from currTimeStep import ModelTime
 import sys
 import logging
+import variable_list
 from reporting import Reporting
 from imagemean import downsample
 from bmi import EBmi
 from bmi import BmiGridType
 import datetime
-import os
+
 
 logger = logging.getLogger(__name__)
 
@@ -133,11 +134,11 @@ class BmiPCRGlobWB(EBmi):
     def get_var_rank(self, long_var_name):
         return 0
 
-    def get_var_size(self, long_var_name):
-        return np.prod(self.get_grid_shape(long_var_name))
+    # def get_var_size(self, long_var_name):
+    #     return np.prod(self.get_grid_shape(long_var_name))
 
     def get_var_nbytes(self, long_var_name):
-        return self.get_var_size(long_var_name) * np.float64.itemsize
+        return self.get_var_size(long_var_name) * np.dtype(np.float64).itemsize
 
     def get_start_time(self):
         return self.days_since_industry_epoch(self.model_time.startTime)
@@ -154,41 +155,41 @@ class BmiPCRGlobWB(EBmi):
     def get_time_units(self):
         return "Days since 1901-01-01"
 
-    def get_value(self, long_var_name):
-        logger.info("getting value for var %s", long_var_name)
+    # def get_value(self, long_var_name):
+    #     logger.info("getting value for var %s", long_var_name)
 
-        if (long_var_name == "top_layer_soil_saturation"):
+    #     if (long_var_name == "top_layer_soil_saturation"):
 
-            if self.model is not None and hasattr(self.model.landSurface, 'satDegUpp000005'):
+    #         if self.model is not None and hasattr(self.model.landSurface, 'satDegUpp000005'):
 
-                #first make all NanS into 0.0 with cover, then cut out the model using the landmask.
-                # This should not actually make a difference.
-                remasked = pcr.ifthen(self.model.landmask, pcr.cover(self.model.landSurface.satDegUpp000005, 0.0))
+    #             #first make all NanS into 0.0 with cover, then cut out the model using the landmask.
+    #             # This should not actually make a difference.
+    #             remasked = pcr.ifthen(self.model.landmask, pcr.cover(self.model.landSurface.satDegUpp000005, 0.0))
 
-                pcr.report(self.model.landSurface.satDegUpp000005, "value.map")
-                pcr.report(remasked, "remasked.map")
+    #             pcr.report(self.model.landSurface.satDegUpp000005, "value.map")
+    #             pcr.report(remasked, "remasked.map")
 
-                value = pcr.pcr2numpy(remasked, np.NaN)
+    #             value = pcr.pcr2numpy(remasked, np.NaN)
 
-            else:
-                logger.info("model has not run yet, returning empty state for top_layer_soil_saturation")
-                value = pcr.pcr2numpy(pcr.scalar(0.0), np.NaN)
+    #         else:
+    #             logger.info("model has not run yet, returning empty state for top_layer_soil_saturation")
+    #             value = pcr.pcr2numpy(pcr.scalar(0.0), np.NaN)
 
-            # print "getting var", value
-            # sys.stdout.flush()
+    #         # print "getting var", value
+    #         # sys.stdout.flush()
 
-            doubles = value.astype(np.float64)
+    #         doubles = value.astype(np.float64)
 
-            # print "getting var as doubles!!!!", doubles
+    #         # print "getting var as doubles!!!!", doubles
 
-            result = np.flipud(doubles)
+    #         result = np.flipud(doubles)
 
-            # print "getting var as doubles flipped!!!!", result
-            # sys.stdout.flush()
+    #         # print "getting var as doubles flipped!!!!", result
+    #         # sys.stdout.flush()
 
-            return result
-        else:
-            raise Exception("unknown var name" + long_var_name)
+    #         return result
+    #     else:
+    #         raise Exception("unknown var name" + long_var_name)
 
     def get_value_at_indices(self, long_var_name, inds):
         raise NotImplementedError
@@ -239,53 +240,10 @@ class BmiPCRGlobWB(EBmi):
                                                                                                 self.model.landSurface.landCoverObj[
                                                                                                     coverType].storUpp000005))
 
-    def set_value(self, long_var_name, src):
 
-        if self.model is None or not hasattr(self.model.landSurface, 'satDegUpp000005'):
-            logger.info("cannot set value for %s, as model has not run yet.", long_var_name)
-            return
-
-        logger.info("setting value for %s", long_var_name)
-
-        # logger.info("dumping state to %s", self.configuration.endStateDir)
-        # self.model.dumpStateDir(self.configuration.endStateDir + "/pre/")
-
-        # print "got value to set", src
-
-        # make sure the raster is the right side up
-        src = np.flipud(src)
-
-        # print "flipped", src
-
-        # cast to pcraster precision
-        src = src.astype(np.float32)
-
-        # print "as float 32", src
-
-        sys.stdout.flush()
-
-        logger.info("setting value shape %s", src.shape)
-
-        if (long_var_name == "top_layer_soil_saturation"):
-            self.set_satDegUpp000005(src)
-        else:
-            raise Exception("unknown var name" + long_var_name)
-
-        # write state here to facilitate restarting tomorrow
-        # logger.info("dumping state to %s", self.configuration.endStateDir)
-        # self.model.dumpStateDir(self.configuration.endStateDir + "/post/")
 
     def set_value_at_indices(self, long_var_name, inds, src):
         raise NotImplementedError
-
-    def get_grid_type(self, long_var_name):
-        return BmiGridType.UNIFORM
-
-    def get_grid_shape(self, long_var_name):
-        return
-
-    def get_grid_shape(self, long_var_name):
-        return self.shape
 
     def get_grid_spacing(self, long_var_name):
 
@@ -304,15 +262,6 @@ class BmiPCRGlobWB(EBmi):
         west = pcr.clone().west()
 
         return np.array([south, west])
-
-    def get_grid_x(self, long_var_name):
-        raise ValueError
-
-    def get_grid_y(self, long_var_name):
-        raise ValueError
-
-    def get_grid_z(self, long_var_name):
-        raise ValueError
 
     def get_grid_connectivity(self, long_var_name):
         raise ValueError
@@ -343,6 +292,219 @@ class BmiPCRGlobWB(EBmi):
 
     def load_state(self, source_directory):
         raise NotImplementedError
+
+    ###
+    # UPDATES TO BMI FUNCTIONS
+    ###
+
+    def get_input_item_count(self) -> int:
+        raise NotImplementedError()
+    
+    def get_output_item_count(self) -> int:
+        raise NotImplementedError()
+
+    def get_var_grid(self, var_name: str) -> int:
+        return 0                                            #test only one grid per var, so should return first index
+        #raise NotImplementedError()
+    
+    def get_var_itemsize(self, var_name: str) -> int:
+        #raise NotImplementedError()
+        var_type = self.get_var_type(var_name)
+        if var_type == 'float64': #should all be this according to get_var_type
+            return 8
+        elif var_type == 'float32':
+            return 4
+        else:
+            return 8  # default
+
+
+        return self.get_var_type(var_name).itemsize
+
+    def get_var_location(self, var_name: str) -> str:
+        raise NotImplementedError()
+    
+    def get_value_ptr(self, var_name: str):
+        raise NotImplementedError()
+    
+    def get_grid_rank(self, grid: int) -> int:
+        if grid == 0:
+            return 2   #2D grid
+        else:
+            raise ValueError(f"Invalid grid: {grid}, debug: should be 0?")   #test
+
+    def get_grid_size(self, grid: int) -> int:
+        if grid != 0:
+            raise ValueError(f"Invalid grid: {grid}, debug: should be 0?")   #test
+        return int(np.prod(self.shape))
+
+        #raise NotImplementedError()
+    
+    def get_grid_shape(self, grid: int, shape: np.ndarray): #https://bmi.csdms.io/en/stable/bmi.grid_funcs.html#get-grid-shape
+        if grid != 0:
+            raise ValueError(f"Invalid grid: {grid}, debug: should be 0?")   #test
+        return self.shape    #ToDo first thing tomorrow, must be self.shape?
+        
+        # should return(?) [rows,columns] = [ny,nx] but has ->None
+        # shape[0] = pcr.clone().nrRows()  #rows = ny
+        # shape[1] = pcr.clone().nrCols()  #columns = nx
+
+        # nrows = int(pcr.clone().nrRows()) #make double sure it's int
+        # ncols = int(pcr.clone().nrCols())
+
+        # shape[0] = nrows
+        # shape[1] = ncols
+    def get_grid_x(self, grid: int, x: np.ndarray) -> np.ndarray:
+        logging.warning("if you see this, get_grid_x is called")    
+        north = pcr.clone().north()
+        cellSize = pcr.clone().cellSize()
+        nrRows = pcr.clone().nrRows()
+        south = north - (cellSize * nrRows)
+        spacing=pcr.clone().cellSize()
+        return south+spacing*(np.arange(nrRows)+0.5)
+        # logging.warning("if you see this swap XY happend")
+        # raise NotImplementedError("if you see this swap XY happend") 
+
+    def get_grid_y(self, grid: int, y: np.ndarray) -> np.ndarray:  #https://github.com/eWaterCycle/PCR-GLOBWB_model/blob/bmi_fixes_setters/model/bmiPcrglobwb.py
+        west = pcr.clone().west()
+        spacing=pcr.clone().cellSize()
+        return west+spacing*(np.arange(self.shape[1])+0.5)
+        #raise NotImplementedError()
+
+    def get_grid_z(self, grid: int, z: np.ndarray) -> np.ndarray:
+        raise NotImplementedError()
+    
+    def get_grid_type(self, grid: int) -> str:
+        return "uniform_rectilinear"
+    
+    def get_var_size(self, long_var_name):   ##
+        grid_id = self.get_var_grid(long_var_name) #should always be 0 for pcrglobwb
+        return self.get_grid_size(grid=grid_id)
+        #return np.prod(self.get_grid_shape(long_var_name))
+
+    def get_value(self, var_name, dest):      #based on:  https://github.com/eWaterCycle/PCR-GLOBWB_model/blob/bmi_fixes_setters/model/bmiPcrglobwb.py
+        logger.info("getting value for var %s", var_name)
+
+        attribute = [n for n in variable_list.netcdf_short_name if variable_list.netcdf_short_name[n] == var_name][0]
+        pcrdata = getattr(self.reporting, attribute)
+
+        # if var_name in BmiPCRGlobWB.scalar_variables:
+        #     dest[:] = float(pcr.pcr2numpy(pcrdata, np.NaN).flat[0])
+        #     return dest
+        #     #return var_name
+        
+        remasked = pcr.ifthen(self.model.landmask, pcr.cover(pcrdata, 0.0))
+        pcr.report(pcrdata, "value.map")
+        pcr.report(remasked, "remasked.map")
+
+        result = np.flipud(pcr.pcr2numpy(remasked, np.NaN))
+        dest[:] = result.flatten()
+        return dest
+    
+    def set_value(self, long_var_name, src):
+
+        if self.model is None:
+            logger.info("cannot set value for %s, as model has not run yet.", long_var_name)
+            return
+
+        logger.info("setting value for %s", long_var_name)
+
+        # logger.info("dumping state to %s", self.configuration.endStateDir)
+        # self.model.dumpStateDir(self.configuration.endStateDir + "/pre/")
+
+        # print "got value to set", src
+
+        src = np.reshape(src, self.shape)
+
+        # make sure the raster is the right side up
+        src = np.flipud(src)
+
+        # print "flipped", src
+
+        # cast to pcraster precision
+        src = src.astype(np.float32)
+
+        # print "as float 32", src
+
+        sys.stdout.flush()
+
+        logger.info("setting value shape %s", src.shape)
+
+        if long_var_name == "near_surface_soil_saturation_degree":
+            self.set_satDegUpp000005(src)
+        # elif long_var_name == "upper_soil_saturation_degree":
+        #     self.set_satDegUpp(src)
+        # elif long_var_name == "precipitation":
+        #     self.set_precipitation(src)
+        # elif long_var_name == "temperature":
+        #     self.set_temperature(src)
+        elif long_var_name == "channel_storage":
+            self.set_channel_storage(src)
+        elif long_var_name == "discharge":
+            self.set_discharge(src)
+
+        else:
+            raise Exception("unknown var name: " + long_var_name)
+        
+        
+    def set_channel_storage(self,src): #https://github.com/eWaterCycle/PCR-GLOBWB_model/blob/bmi_fixes_setters/model/bmiPcrglobwb.py
+        mask = np.isnan(src)
+        src[mask] = 1e20
+        channel_storage = pcr.numpy2pcr(pcr.Scalar, src, 1e20)
+
+        channel_storage = pcr.ifthen(self.model.meteo.landmask, channel_storage)
+        #-----------------------------------------------------------------------
+
+        channel_storage = pcr.max(0., channel_storage)
+        channel_storage = pcr.cover( channel_storage, 0.0)
+                
+        self.model.routing.channelStorage = channel_storage
+
+        self.reporting.channel_storage = pcr.ifthen(self.model.routing.landmask, self.model.routing.channelStorage)
+
+    def set_discharge(self,src): #based on https://github.com/eWaterCycle/PCR-GLOBWB_model/blob/bmi_fixes_setters/model/bmiPcrglobwb.py
+        mask = np.isnan(src)
+        src[mask] = 1e20
+        discharge = pcr.numpy2pcr(pcr.Scalar, src, 1e20)
+
+        discharge = pcr.ifthen(self.model.meteo.landmask, discharge)
+        #-----------------------------------------------------------------------
+
+        discharge = pcr.max(0., discharge)
+        discharge = pcr.cover( discharge, 0.0)
+                
+        self.model.routing.discharge = discharge
+
+        self.reporting.discharge = pcr.ifthen(self.model.routing.landmask, self.model.routing.discharge)
+
+
+    
+
+
+
+
+
+
+    
+    def get_grid_node_count(self, grid: int) -> int:
+        raise NotImplementedError()
+
+    def get_grid_edge_count(self, grid: int) -> int:
+        raise NotImplementedError()
+    
+    def get_grid_face_count(self, grid: int) -> int:
+        raise NotImplementedError()
+
+    def get_grid_edge_nodes(self, grid: int, edge_nodes: np.ndarray) -> np.ndarray:
+        raise NotImplementedError()
+
+    def get_grid_face_edges(self, grid: int, face_edges: np.ndarray) -> np.ndarray:
+        raise NotImplementedError()
+
+    def get_grid_face_nodes(self, grid: int, face_nodes: np.ndarray) -> np.ndarray:
+        raise NotImplementedError()
+
+    def get_grid_nodes_per_face(self, grid: int, nodes_per_face: np.ndarray) -> np.ndarray:
+        raise NotImplementedError()
 
 
 
